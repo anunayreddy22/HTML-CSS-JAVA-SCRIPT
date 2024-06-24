a4cf0359a0f68e73d3b0c9b0d922143e4c8ba6e2
anunayreddy22 <156383908+anunayreddy22@users.noreply.github.com>
2024-06-24 11:13:38 -0500

    Update and rename README.md to README.java

--- README.java
+++ README.java
@@ -1,13 +1,13 @@
 HTML-CSS-JAVA-SCRIPT
 
-<!DOCTYPE html> -->  Declaration defines that this document is an HTML5 document
+(<!DOCTYPE html>) -->  Declaration defines that this document is an HTML5 document
 <html>   --> Tag open   this is the root element of an HTML page
 <head>   ->  contains meta information about the HTML page
 <title>Page Title</title>    --> specifies a title for the HTML page (which is shown in the browser's title bar or in the page's tab)
 </head>
 <body>     ->  a container for all the visible contents, such as headings, paragraphs, images, hyperlinks, tables, lists, etc.
 
-<h1>This is a Heading</h1>  ->  <h1> defines the most important heading. <h6> defines the least important heading: 
+(<h1>This is a Heading</h1>)  ->  <h1> defines the most important heading. (h6) defines the least important heading: 
 <p>This is a paragraph.</p> -> Words inside These Tage will be in normal words.
 
 </body>  The visible part of the HTML document is between <body> and </body>
